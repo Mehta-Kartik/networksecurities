c1cc3ef664b4520c7246a2bc34316e1ac7b961ac
Mehta-Kartik <24cskar010@ldce.ac.in>
2026-04-03 16:51:16 +0530

    Integrated Model Training and Evaluation

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging import logger
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
-
+from networksecurity.components.model_trainer import ModelTrainer
 
 if __name__=="__main__":
     try:
@@ -37,5 +37,13 @@ if __name__=="__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         logger.info("Data Transformation Completed")
         print(data_transformation_artifact)
+
+
+        #Model Trainer
+        logger.info("Model Training Started")
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logger.info("Model Training completed")
     except Exception as e:
         raise NetworkSecurityException(e,sys)   
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -5,6 +5,8 @@ import os,sys
 import numpy as np
 import dill
 import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 def read_yaml_file(file_path:str) ->dict:
     try:
@@ -51,5 +53,54 @@ def save_object(file_path:str,obj:object)->None:
             pickle.dump(obj,file_obj)
 
         logger.info("Exited the save_object method of MainUtils class")
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def load_object(file_path:str,)->object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} is not exists")
+        with open(file_path,'rb') as fobj:
+            print(fobj)
+            return pickle.load(fobj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def load_numpy_array_data(file_path:str)->object:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path,"rb") as fileobj:
+            return np.load(fileobj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            # CORRECT - iterate over dict items
+            for model_name, model in models.items():
+                # ... train and predict ...
+                report[model_name] = test_model_score  # Use model_name directly
+        return report
     except Exception as e:
         raise NetworkSecurityException(e,sys)
